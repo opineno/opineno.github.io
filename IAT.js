34b6f8e7bb5a57e023f84ca432b22e7f633cddc0
opineno <148588847+opineno@users.noreply.github.com>
2023-10-20 20:07:55 -0400

    Update IAT.js

--- IAT.js
+++ IAT.js
@@ -39,7 +39,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/opineno/opineno.github.io@main/fu
 		},	
 
 		base_url : {//Where are your images at?
-			image : 'https://baranan.github.io/minno-tasks/images/'
+			image : 'https://opineno.github.io/images/'
 		} 
 	});
 });
