857080a0b60972fb0a17613cba1b8a3977a5f811
opineno <148588847+opineno@users.noreply.github.com>
2023-11-01 16:22:45 -0400

    Update foodIAT_words.js

--- foodIAT_words.js
+++ foodIAT_words.js
@@ -3,8 +3,28 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/opineno/opineno.github.io@main/fu
 
 	return iatExtension({
 		//OP note: This overrides the categories in fullscript.js
+		//OP note: category1 and category2 now inverted to match their compatible attributes in the main source code
 
-		category1 : {
+		category1 :	
+		{
+			name : 'Junk food', //Will appear in the data.
+			title : {
+				media : {word : 'Junk food'}, //Name of the category presented in the task.
+				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+				height : 4 //Used to position the "Or" in the combined block.
+			}, 
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+    		  	{word : 'Potato chips'}, 
+    			{word : 'French fries'}, 
+    			{word : 'Fried chicken'}, 
+    			{word : 'Pizza'}, 
+    			{word : 'Soda'}, 
+    			{word : 'Candy bar'}			], 
+			//Stimulus css
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},	
+		category2 : 
+		{
 			name : 'Healthy food', //Will appear in the data.
 			title : {
 				media : {word : 'Healthy food'}, //Name of the category presented in the task.
@@ -20,23 +40,6 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/opineno/opineno.github.io@main/fu
     			{word : 'Broccoli'}
 			], 
 			//Stimulus css (style)
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		},	
-		category2 :	{
-			name : 'Junk food', //Will appear in the data.
-			title : {
-				media : {word : 'Junk food'}, //Name of the category presented in the task.
-				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-				height : 4 //Used to position the "Or" in the combined block.
-			}, 
-			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		  	{word : 'Potato chips'}, 
-    			{word : 'French fries'}, 
-    			{word : 'Fried chicken'}, 
-    			{word : 'Pizza'}, 
-    			{word : 'Soda'}, 
-    			{word : 'Candy bar'}			], 
-			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},
 
