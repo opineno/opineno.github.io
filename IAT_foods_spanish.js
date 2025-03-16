27d53f313922875a5c2ff011e7870f7ad1f9bef0
opineno <148588847+opineno@users.noreply.github.com>
2025-03-16 15:39:02 -0400

    Update and rename IAT_foods_SP.js to IAT_foods_spanish.js

--- IAT_foods_spanish.js
+++ IAT_foods_spanish.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/opineno/opineno.github.io@main/fullscript_SP.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/opineno/opineno.github.io@main/fullscript_spanish.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
