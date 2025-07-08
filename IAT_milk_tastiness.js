f2eabce4cb517d933b43fc91788a3a76f4e841f9
opineno <148588847+opineno@users.noreply.github.com>
2025-07-08 15:40:10 -0400

    Create IAT_milk_tastiness.js

---
+++ IAT_milk_tastiness.js
@@ -0,0 +1,47 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/opineno/opineno.github.io@main/fullscript.js'], function(APIConstructor, iatExtension){
+    var API = new APIConstructor();
+
+	return iatExtension({
+                //OP note: This overrides the categories in fullscript.js
+		//OP note: category1 and category2 now inverted to match their compatible attributes in the main source code
+    
+          	category1 : {
+			name : 'Dairy milk', //Will appear in the data.
+		        title : {
+	          	media : {word : 'Dairy milk'}, //Name of the category presented in the task.
+	          	css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+          		height : 4 //Used to position the "Or" in the combined block.
+            		}, 
+            		stimulusMedia : [ //Stimuli content as PIP's media objects
+    		    	{image : 'Cow1.jpg'}, 
+        		{image : 'Cow2.jpg'}, 
+        		{image : 'Cow3.jpg'}, 
+        		{image : 'Cow4.jpg'}, 
+        		{image : 'Cow5.jpg'}, 
+        	        {image : 'Cow6.jpg'}  ], 
+        	    	//Stimulus css
+            		stimulusCss : {color:'#31940F','font-size':'1.8em'}
+	      	      },
+          	category2 : {
+			name : 'Plant-based milk', //Will appear in the data.
+		        title : {
+		        media : {word : 'Plant-based milk'}, //Name of the category presented in the task.
+		        css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+			height : 4 //Used to position the "Or" in the combined block.
+		        }, 
+		        stimulusMedia : [ //Stimuli content as PIP's media objects
+    		        {image : 'NoCow1.jpg'}, 
+    	        	{image : 'NoCow2.jpg'}, 
+    	        	{image : 'NoCow3.jpg'}, 
+            		{image : 'NoCow4.jpg'}, 
+            		{image : 'NoCow5.jpg'}, 
+    	        	{image : 'NoCow6.jpg'}  ], 
+			//Stimulus css (style)
+			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		      },	
+    
+		base_url : {//Where are your images at?
+			image : 'https://opineno.github.io/IAT_foods/images/'
+		} 
+	});
+});
