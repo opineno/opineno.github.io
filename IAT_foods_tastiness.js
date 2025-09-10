c81b5f00bf81de32bf5ffedfad3d7120824d3375
opineno <148588847+opineno@users.noreply.github.com>
2025-09-10 08:36:41 -0400

    Update IAT_foods_tastiness.js

--- IAT_foods_tastiness.js
+++ IAT_foods_tastiness.js
@@ -8,27 +8,27 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/opineno/opineno.github.io@main/fu
 		category1 : {
 			name : 'Junk food', //Will appear in the data.
 		        title : {
-	          	media : {word : 'Junk food'}, //Name of the category presented in the task.
-	          	css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-          		height : 4 //Used to position the "Or" in the combined block.
+		          	media : {word : 'Junk food'}, //Name of the category presented in the task.
+		          	css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+	          		height : 4 //Used to position the "Or" in the combined block.
             		}, 
-            		stimulusMedia : [ //Stimuli content as PIP's media objects
+            	stimulusMedia : [ //Stimuli content as PIP's media objects
     		    	{image : 'Potato_chips.jpg'}, 
-        		{image : 'French_fries.jpg'}, 
-        		{image : 'Fried_chicken.jpg'}, 
-        		{image : 'Pizza.jpg'}, 
-        		{image : 'Soda.jpg'}, 
+	        		{image : 'French_fries.jpg'}, 
+	        		{image : 'Fried_chicken.jpg'}, 
+	        		{image : 'Pizza.jpg'}, 
+	        		{image : 'Soda.jpg'}, 
         	        {image : 'Candy_bar.jpg'}  ], 
         	    	//Stimulus css
             		stimulusCss : {color:'#31940F','font-size':'1.8em'}
-	      	      },
+	      	    },
 		category2 : {
 			name : 'Healthy food', //Will appear in the data.
 		        title : {
-		        media : {word : 'Healthy food'}, //Name of the category presented in the task.
-		        css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
-			height : 4 //Used to position the "Or" in the combined block.
-		        }, 
+			        media : {word : 'Healthy food'}, //Name of the category presented in the task.
+			        css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
+					height : 4 //Used to position the "Or" in the combined block.
+			        }, 
 		        stimulusMedia : [ //Stimuli content as PIP's media objects
     		        {image : 'Almonds.jpg'}, 
     	        	{image : 'Blueberries.jpg'}, 
@@ -36,44 +36,44 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/opineno/opineno.github.io@main/fu
             		{image : 'Eggs.jpg'}, 
             		{image : 'Avocado.jpg'}, 
     	        	{image : 'Broccoli.jpg'}  ], 
-			//Stimulus css (style)
-			stimulusCss : {color:'#31940F','font-size':'1.8em'}
-		      },
+					//Stimulus css (style)
+					stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		    	},
 		attribute1 : {
 			name : 'Not Enjoyable',
-			title : {
-			media : {word : 'Not Enjoyable'},
-			css : {color:'#0000FF','font-size':'1.8em'},
-			height : 4 //Used to position the "Or" in the combined block.
-			},
+				title : {
+				media : {word : 'Not Enjoyable'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+				},
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-			{word: 'disliked'},
+				{word: 'disliked'},
       			{word: 'less tasty'},
-			{word: 'unappealing'},
-			{word: 'bland'},
-			{word: 'flavorless'},
-			{word: 'unappetizing'},
-			{word: 'unpalatable'}	],
-			//Stimulus css
-			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+				{word: 'unappealing'},
+				{word: 'bland'},
+				{word: 'flavorless'},
+				{word: 'unappetizing'},
+				{word: 'unpalatable'}	],
+				//Stimulus css
+				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
 			},
 		attribute2 : {
 			name : 'Enjoyable',
-			title : {
-			media : {word : 'Enjoyable'},
-			css : {color:'#0000FF','font-size':'1.8em'},
-			height : 4 //Used to position the "Or" in the combined block.
-			},
+				title : {
+				media : {word : 'Enjoyable'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+				},
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-			{word: 'tasty'},
-			{word: 'delicious'},
-			{word: 'yummy'},
-			{word: 'appetizing'},
-			{word: 'flavorful'},
-			{word: 'appealing'},
-			{word: 'mouthwatering'}	],
-			//Stimulus css
-			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+				{word: 'tasty'},
+				{word: 'delicious'},
+				{word: 'yummy'},
+				{word: 'appetizing'},
+				{word: 'flavorful'},
+				{word: 'appealing'},
+				{word: 'mouthwatering'}	],
+				//Stimulus css
+				stimulusCss : {color:'#0000FF','font-size':'2.3em'}
 			},
 
 		base_url : {//Where are your images at?
