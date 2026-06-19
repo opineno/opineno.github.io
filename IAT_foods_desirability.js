eb12c36e45b85e9fcdf86cf7b374ecd71cc2577c
opineno <148588847+opineno@users.noreply.github.com>
2026-06-19 09:31:15 -0400

    Update script source to fullscript_TY_keys.js

--- IAT_foods_desirability.js
+++ IAT_foods_desirability.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/opineno/opineno.github.io@main/fullscript.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/opineno/opineno.github.io@main/fullscript_TY_keys.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
